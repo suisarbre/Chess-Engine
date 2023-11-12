98e04731be85a61f07c53817199c1b096bc5e4e3
suisarbre <itoroc0417@gmail.com>
2023-11-12 02:36:16 -0800

    board drawn

--- ChessMain.py
+++ ChessMain.py
@@ -10,18 +10,49 @@ pg.init()
 WIDTH = HEIGHT = 512
 DIMENTION = 8
 SQ_SIZE = HEIGHT // DIMENTION
-MAX_FPS
+MAX_FPS = 15
 IMAGES = {}
 
 
 def loadImages():
-    pieces = ["bR","bN","bB","bQ","bK","bB","bN","bR","bp"]
-    pieces += ["wR","wN","wB","wQ","wK","wB","wN","wR","wp"]
+    pieces = ["bR","bN","bB","bQ","bK","bB","bN","bR","bp","wR","wN","wB","wQ","wK","wB","wN","wR","wp"]
     for piece in pieces:
-        IMAGES[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"),SQ_SIZE,SQ_SIZE)
+        IMAGES[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"),(SQ_SIZE,SQ_SIZE))
         
 def main():
     screen = pg.display.set_mode((WIDTH,HEIGHT))
     clock = pg.time.Clock()
     screen.fill(pg.Color("white"))
     gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    
+    while running:#game loop
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                running = False
+        drawGameState(screen,gs)
+        clock.tick(MAX_FPS)
+        pg.display.flip()
+
+#Graphics
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+#Draws squares
+def drawBoard(screen):
+    colors = [pg.Color("white"),pg.Color("gray")]
+    for r in range(DIMENTION):
+        for c in range(DIMENTION):
+            color = colors[(r+c)%2]
+            pg.draw.rect(screen,color,pg.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+    pass
+    
+#Draws pieces based on the gamestate
+def drawPieces(screen,board):
+    pass
+    
+
+if __name__ == "__main__":
+    main()
